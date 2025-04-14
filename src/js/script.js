202da4a1f8100b12f2822f9ea68635ecba156403
Artem <ap3994741@gmail.com>
2025-04-14 18:56:39 +0300

    created activities section

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,4 @@
+import "bootstrap/scss/bootstrap-grid.scss"
+import "bootstrap/scss/bootstrap-utilities.scss" // bootstrap 
+
 import "/src/sass/style.scss";
-import * as bootstrap from 'bootstrap';
