71361735d55ad6a43ee163bafc6e1aa12778bf4e
Artem <ap3994741@gmail.com>
2025-04-18 23:04:58 +0300

    created works section and  start work with slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import "/src/sass/style.scss";
+
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination]
+  });
+
